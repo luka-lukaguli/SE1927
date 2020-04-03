5feda142de18e6de4970847e80fdfb4560ac22d1
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-04-03 20:27:40 +0400

    Linked List Added; AddFirst NOT IMPLEMENTED

---
+++ DataStructures/DataStructures/SinglyLinkedList.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+struct Node
+{
+	double _value;
+	Node* _next = nullptr;
+
+	Node(double value)
+	{
+		_value = value;
+	}
+};
+
+class SinglyLinkedList
+{
+private:
+	Node* _head = nullptr;
+	Node* _tail = nullptr;
+	int _count = 0;
+
+public:
+	void AddLast(double item)
+	{
+		Node* newNode = new Node(item);
+
+		if (_head == nullptr && _tail == nullptr)
+		{
+			_head = newNode;
+		}
+		else
+		{
+			_tail->_next = newNode;
+		}
+		_tail = newNode;
+		_count++;
+	}
+
+	void AddFirst(double item)
+	{
+		Node* newNode = new Node(item);
+
+		// დაასრულეთ თავში დამატება
+	}
+};
--- DataStructures/DataStructures/Source.cpp
+++ DataStructures/DataStructures/Source.cpp
@@ -4,6 +4,7 @@
 #include "List.h"
 #include "Array.h"
 #include "String.h"
+#include "SinglyLinkedList.h"
 
 using namespace std;
 
@@ -16,66 +17,6 @@ void PrintArray(const List<T>& array)
 	}
 	cout << endl;
 }
-
-//struct Product
-//{
-//public:
-//	string name;
-//	string description;
-//
-//private:
-//	double _price;
-//	double _discount;
-//	int _quantity;
-//
-//public:
-//
-//	Product(double _price)
-//	{
-//		_price = _price;
-//	}
-//
-//	// Getter
-//	double GetPrice() const { return _price; }
-//
-//	// Getter
-//	double GetDiscount() const { return _discount; }
-//
-//	// Getter
-//	int GetQuantity() const { return _quantity; }
-//
-//	// Getter
-//	double GetDiscountedPrice() const { return _price - _price * _discount; }
-//
-//	// Setter
-//	void SetPrice(double price)
-//	{
-//		if (price >= 0)
-//			_price = price;
-//	}
-//
-//	// Setter
-//	void SetDiscount(double discount)
-//	{
-//		if (discount >= 0 && discount < 1)
-//			_discount = discount;
-//	}
-//
-//	// Setter
-//	void SetQuantity(int quantity)
-//	{
-//		if (quantity >= 0)
-//			_quantity = quantity;
-//	}
-//};
-//
-//void PrintProduct(const Product& prod)
-//{
-//	cout << "Price          : " << prod.GetPrice() << endl;
-//	cout << "Discount       : " << prod.GetDiscount() << endl;
-//	cout << "Quantity       : " << prod.GetQuantity() << endl;
-//	cout << "DiscountedPrice: " << prod.GetDiscountedPrice() << endl;
-//}
 
 struct Student
 {
@@ -109,7 +50,7 @@ void PrintStudent(const Student& student)
 	cout << "\n------------------------------\n";
 }
 
-int main()
+void ProcessStudents()
 {
 	String line;
 
@@ -125,6 +66,11 @@ int main()
 
 	students.Sort([](Student s1, Student s2) {return s1.firstName.Compare(s2.firstName) == -1; });
 	students.Foreach([](int i, const Student& s) {PrintStudent(s); });
+}
+
+int main()
+{
+	// ProcessStudents();
 
 	return 0;
 }
