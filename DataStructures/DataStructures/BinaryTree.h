59308d652f67ffb1dd28a9f5085b055ce42544d1
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-04-15 21:38:32 +0400

    Binary Tree Finished

--- DataStructures/DataStructures/BinaryTree.h
+++ DataStructures/DataStructures/BinaryTree.h
@@ -26,6 +26,8 @@ private:
 
 public:
 
+	int Count() { return count; }
+
 	bool Add(TKey key, TValue value)
 	{
 		if (root == nullptr)
@@ -111,5 +113,135 @@ public:
 		TraverseInOrder(theRoot->Left, a);
 		a(theRoot);
 		TraverseInOrder(theRoot->Right, a);
+	}
+
+	Node* BinarySearch(TKey key)
+	{
+		return BinarySearch(key, root);
+	}
+
+	// ReSharper disable once CppNotAllPathsReturnValue
+	Node* BinarySearch(TKey key, Node* current, Node*& parent)
+	{
+		if (current == nullptr)
+			return nullptr;
+
+		if (key == current->Key)
+			return current;
+
+		parent = current;
+		if (key < current->Key)
+			return BinarySearch(key, current->Left, parent);
+
+		if (key > current->Key)
+			return BinarySearch(key, current->Right, parent);
+	}
+
+	bool IsLeaf(Node* node)
+	{
+		return node->Right == nullptr && node->Left == nullptr;
+	}
+
+	bool IsSingleParent(Node* node)
+	{
+		return !IsDoubleParent(node) && !IsLeaf(node);
+	}
+
+	bool IsDoubleParent(Node* node)
+	{
+		return node->Right != nullptr && node->Left != nullptr;
+	}
+
+	void Delete(TKey key)
+	{
+		auto parent = root;
+		auto nodeTodelete = BinarySearch(key, root, parent);
+
+		DeleteNode(nodeTodelete, parent);
+	}
+
+	void DeleteNode(Node* nodeToDelete, Node* parentNode)
+	{
+		if (nodeToDelete == nullptr)
+			return;
+
+		if (IsLeaf(nodeToDelete))
+			DeleteLeaf(nodeToDelete, parentNode);
+		else if (IsSingleParent(nodeToDelete))
+			DeleteSingleParent(nodeToDelete, parentNode);
+		else if (IsDoubleParent(nodeToDelete))
+			DeleteDoubleParent(nodeToDelete, parentNode);
+		count--;
+	}
+
+	void DeleteLeaf(Node* nodeToDelete, Node* parentNode)
+	{
+		if (root == nodeToDelete)
+			root = nullptr;
+		else if (parentNode->Left == nodeToDelete)
+			parentNode->Left = nullptr;
+		else
+			parentNode->Right = nullptr;
+
+		delete nodeToDelete;
+	}
+
+	void DeleteSingleParent(Node* nodeToDelete, Node* parentNode)
+	{
+		auto childNode = nodeToDelete->Left != nullptr ? nodeToDelete->Left : nodeToDelete->Right;
+
+		if (nodeToDelete == root)
+		{
+			root = childNode;
+			delete nodeToDelete;
+			return;
+		}
+
+		if (parentNode->Left == nodeToDelete)
+			parentNode->Left = childNode;
+		else
+			parentNode->Right = childNode;
+
+		delete nodeToDelete;
+	}
+
+	void DeleteDoubleParent(Node* nodeToDelete, Node* parentNode)
+	{
+		auto substituteParent = nodeToDelete->Right;
+		auto substitute = Min(nodeToDelete->Right, substituteParent);
+		if (substitute != substituteParent)
+			substituteParent->Left = substitute->Right;
+
+		substitute->Left = nodeToDelete->Left;
+		substitute->Right = nodeToDelete->Right;
+
+		if (nodeToDelete == root)
+		{
+			root = substitute;
+		}
+		else
+		{
+			if (parentNode->Left == nodeToDelete)
+				parentNode->Left = substitute;
+			else
+				parentNode->Right = substitute;
+		}
+		delete nodeToDelete;
+	}
+
+	Node* Min(Node* current, Node*& parent)
+	{
+		if (current->Left == nullptr)
+			return current;
+		parent = current;
+		return Min(current->Left, parent);
+	}
+
+	Node* Max(Node* current, Node*& parent)
+	{
+		if (current->Right == nullptr)
+			return current;
+		parent = current;
+		return Max(current->Right, parent);
 	}
 };
--- DataStructures/DataStructures/Source.cpp
+++ DataStructures/DataStructures/Source.cpp
@@ -1,51 +1,46 @@
 ﻿#include <iostream>
+#include <string>
 #include "BinaryTree.h"
 
 using namespace std;
 
-void Function(int number)
+template <typename TNode>
+void PrintNode(TNode node)
 {
-	if (number == 0)
-		return;
-	Function(number - 1);
-	cout << "Inside Function " << number << endl;
+	cout << "{ " << node->Key << " : " << node->Value << " }" << endl;
 }
 
-int Factorial(int number)
+void Print(BinaryTree<int, int>::Node* node)
 {
-	if (number == 0)
-		return 1;
-	return number * Factorial(number - 1);
-}
-
-int Sum(int number)
-{
-	if (number == 0)
-		return 0;
-	return number + Sum(number - 1);
-}
-typedef BinaryTree<int, double>::Node* node;
-
-void Print(node n)
-{
-	cout << n->Key << " ";
+	cout << node->Key << " ";
 }
 
 int main()
 {
-	BinaryTree<int, double> numbers;
+	BinaryTree<int, int> numbers;
 
 	numbers.Add(25, 0);
-	numbers.Add(10, 0);
 	numbers.Add(30, 0);
+	numbers.Add(10, 0);
 	numbers.Add(3, 0);
+	numbers.Add(17, 0);
 
-	numbers.TraverseInOrder([](node n) {cout << n->Key << " "; });
-	cout << "\n------------------------\n";
-	numbers.TraversePreOrder([](node n) {cout << n->Key << " "; });
-	cout << "\n------------------------\n";
+	numbers.Add(27, 0);
+	numbers.Add(26, 0);
+	numbers.Add(29, 0);
 
-	numbers.TraversePostOrder([](node n) {cout << n->Key << " "; });
+	numbers.Add(55, 0);
+	numbers.Add(53, 0);
+	numbers.Add(77, 0);
+	numbers.Add(54, 0);
+	numbers.Add(100, 0);
+	numbers.Add(32, 0);
+
+	numbers.TraverseInOrder(Print);
+	cout << endl;
+	numbers.Delete(30);
+	numbers.Delete(25);
+	numbers.TraverseInOrder(Print);
 
 	return 0;
 }
